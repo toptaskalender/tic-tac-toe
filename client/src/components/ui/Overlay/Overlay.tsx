261691b5de982a376038a38dbef51fd28f85e533
Kalender Toptas <toptaskalender@gmail.com>
2023-10-17 17:10:13 +0300

    refactor: change overlay style

--- client/src/components/ui/Overlay/Overlay.tsx
+++ client/src/components/ui/Overlay/Overlay.tsx
@@ -2,7 +2,7 @@ import { createPortal } from 'react-dom';
 
 function Overlay() {
   return createPortal(
-    <div className='fixed inset-0 left-96' />,
+    <div className='fixed inset-0 left-96 bg-black/20 backdrop-blur-sm' />,
     document.getElementById('overlay-root')!,
   );
 }
