ca47c7f27ecdd648793f376b0d3f1661e2337619
Kalender Toptas <toptaskalender@gmail.com>
2023-10-18 16:17:09 +0300

    refactor: folder structure

--- server/constants/CONFIGS.js
+++ server/constants/CONFIGS.js
@@ -1,4 +1,9 @@
-export const BASE_CLIENT_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.BASE_CLIENT_URL
-    : "http://localhost:5173";
+const CONFIGS = {
+  PORT: process.env.PORT || 3000,
+  BASE_CLIENT_URL:
+    process.env.NODE_ENV === "production"
+      ? process.env.BASE_CLIENT_URL
+      : "http://localhost:5173",
+};
+
+export default CONFIGS;
--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 
-import { BASE_CLIENT_URL } from "../constants/CONFIGS.js";
+import CONFIGS from "./constants/CONFIGS.js";
 
 const app = express();
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: BASE_CLIENT_URL,
+    origin: CONFIGS.BASE_CLIENT_URL,
   },
 });
 
@@ -21,6 +21,6 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
-server.listen(3000, () => {
+server.listen(CONFIGS.PORT, () => {
   console.log("server running at http://localhost:3000");
 });
